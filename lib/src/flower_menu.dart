f171439cdb960ec3029c80b76b877e8c7598d78a
MalanDev <malandev95@gmail.com>
2024-10-23 21:10:46 +0530

    added example project

---
+++ lib/src/flower_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlowerMenuContainer extends StatefulWidget {
+  const FlowerMenuContainer({super.key});
+
+  @override
+  State<FlowerMenuContainer> createState() => _FlowerMenuContainerState();
+}
+
+class _FlowerMenuContainerState extends State<FlowerMenuContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
